eb28231cda1eb74d170a090c34b4631b2e4eb0d6
Ximena Becerril Perez <xbecerri@villanova.edu>
2024-01-26 22:23:43 -0500

    word checking feature completed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
 
-    const keys = document.querySelectorAll('keyboard-row button')
+    const guessedWords = [[]];
 
-    for (let inde)
+    let availableSpace = 1;
+
+    let word = "dairy";
+
+    const keys = document.querySelectorAll('.keyboard-row button');
+
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute('data-key');
+            updateGuessedWords(letter);
+        };
+    }
+
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords - 1];
+    }
+
+    function updateGuessedWords(letter) {
+        const currentWordArr = getCurrentWordArr();
+
+        if (currentWordArr && currentWordArr.length < 5) {
+            currentWordArr.push(letter);
+
+            const availableSpaceEl = document.getElementById(String(availableSpace));
+            availableSpace = availableSpace + 1;
+
+            availableSpaceEl.textContent = letter;
+        }
+    }
+
+    function handleSubmitWord() {
+        const currentWordArr = getCurrentWordArr();
+        if(currentWordArr.length !== 5){
+            window.alert("Word must be 5 letters friend")
+        }
+
+        const currentWord = currentWordArr.join('');
+
+        if(currentWord === word){
+            window.alert("Yay, you did it!");
+        }
+
+        if(guessedWords.length === 6){
+            window.alert(`This guy stinks! The word was ${word}.`);
+        }
+
+        guessedWords.push([]);
+    }
+
     function createSquares() {
         const gameBoard = document.getElementById("board");
 
@@ -15,5 +64,15 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute('data-key');
 
+            if(letter === 'enter'){
+                handleSubmitWord();
+                return;
+            }
+            updateGuessedWords(letter);
+        };
+    }
 });
