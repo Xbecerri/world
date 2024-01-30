3b1e36af5894c531e93825b4aa74bcbe733100bf
Ximena Becerril Perez <xbecerri@villanova.edu>
2024-01-29 19:54:22 -0500

    delete button is now functional

--- js/main.js
+++ js/main.js
@@ -1,163 +1,6 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     createSquares();
-
-//     const guessedWords = [[]];
-
-//     let availableSpace = 1;
-
-//     let word;
-
-//     let guessedWordCount = 0;
-//     const keys = document.querySelectorAll('.keyboard-row button');
-
-//     function getNewWord() {
-//         const url = 'https://wordsapiv1.p.rapidapi.com/words/';
-//         const params = new URLSearchParams({
-//             random: 'true',
-//             lettersMin: '5',
-//             lettersMax: '5'
-//         });
-    
-//         fetch(`${url}?${params.toString()}`, {
-//             method: "GET",
-//             headers: {
-//                 "X-RapidAPI-Key": "6e9de6409emsh941e7b01617e467p1b5ee3jsnbd6ad0832559",
-//                 "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
-//             },
-//         })
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((res) => {
-//             word = res.word;
-//             console.log(word);
-//         })
-//         .catch((err) => {
-//             console.error(err);
-//         });
-//     }
-
-//     for (let i = 0; i < keys.length; i++) {
-//         keys[i].onclick = ({ target }) => {
-//             const letter = target.getAttribute('data-key');
-//             updateGuessedWords(letter);
-//         };
-//     }
-
-//     function getCurrentWordArr() {
-//         const numberOfGuessedWords = guessedWords.length;
-//         return guessedWords[numberOfGuessedWords - 1];
-//     }
-
-//     function updateGuessedWords(letter) {
-//         const currentWordArr = getCurrentWordArr();
-
-//         if (currentWordArr && currentWordArr.length < 5) {
-//             currentWordArr.push(letter);
-
-//             const availableSpaceEl = document.getElementById(String(availableSpace));
-//             availableSpace = availableSpace + 1;
-
-//             availableSpaceEl.textContent = letter;
-//         }
-//     }
-
-//     function getTileColor(letter, index){
-//         const isCorrectLetter = word.includes(letter);
-
-//         if(!isCorrectLetter){
-//             return "rgb(58, 58, 60)";
-//         }
-    
-
-//     const letterInThatPosition = word.charAt(index);
-//     const isCorrectPosition = letter === letterInThatPosition;
-
-//     if(isCorrectPosition){
-//         return "rgb(83, 141, 78)";
-//     }
-
-//     return "rgb(181, 159, 59)";
-//     }
-//     function handleSubmitWord() {
-//         const currentWordArr = getCurrentWordArr();
-//         if(currentWordArr.length !== 5){
-//             window.alert("Word must be 5 letters friend")
-//         }
-
-//         const currentWord = currentWordArr.join("");
-
-//         fetch(
-//             `https://wordsapiv1.p.rapidapi.com/words/{currentWord}`,
-//             {
-//                 method: "GET",
-//                 headers: {
-//                     "X-Rapidapi-Key": "6e9de6409emsh941e7b01617e467p1b5ee3jsnbd6ad0832559",
-//                     "X-Rapidapi-Host": "wordsapiv1.p.rapidapi.com",
-//                 },
-//             }
-//         ).then((res) => {
-//             if (!res.ok) {
-//                 throw Error()
-//             }
-//             const firstLetterId = guessedWordCount * 5 + 1;
-//             const interval = 200;
-//             currentWordArr.forEach((letter, index) => {
-//                 setTimeout(() => {
-//                     const tileColor = getTileColor(letter, index);
-    
-//                     const letterId = firstLetterId + index; 
-//                     const letterEl = document.getElementById(letterId);
-//                     letterEl.classList.add("animate__flipInX");
-//                     letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
-//                 }, interval * index);
-//                 })
-            
-//             guessedWordCount += 1;
-//             if(currentWord === word){
-//                 window.alert("Yay, you did it!");
-//             }
-    
-//             if(guessedWords.length === 6){
-//                 window.alert(`This guy stinks! The word was ${word}.`);
-//             }
-    
-//             guessedWords.push([]);
-
-//         }).catch(() => window.alert("Not a word"))
-
-
-//     }
-
-//     function createSquares() {
-//         const gameBoard = document.getElementById("board");
-
-//         for (let index = 0; index < 30; index++) {
-//             let square = document.createElement("div");
-//             square.classList.add("square");
-//             square.classList.add("animate__animated");
-//             square.setAttribute("id", index + 1);
-//             gameBoard.appendChild(square);
-//         }
-//     }
-
-//     for (let i = 0; i < keys.length; i++) {
-//         keys[i].onclick = ({ target }) => {
-//             const letter = target.getAttribute('data-key');
-
-//             if(letter === 'enter'){
-//                 handleSubmitWord();
-//                 return;
-//             }
-//             updateGuessedWords(letter);
-//         };
-//     }
-// });
-
-
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
-    getNewWord(); // Fetch the new word when the DOM is loaded
+    getNewWord(); 
 
     const guessedWords = [[]];
 
@@ -166,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let word;
 
     let guessedWordCount = 0;
+
     const keys = document.querySelectorAll('.keyboard-row button');
 
     function getNewWord() {
@@ -207,6 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
         return guessedWords[numberOfGuessedWords - 1];
     }
 
+
     function updateGuessedWords(letter) {
         const currentWordArr = getCurrentWordArr();
 
@@ -246,7 +91,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const currentWord = currentWordArr.join("");
 
         fetch(
-            `https://wordsapiv1.p.rapidapi.com/words/{currentWord}`,
+            `https://wordsapiv1.p.rapidapi.com/words/${currentWord}`,
             {
                 method: "GET",
                 headers: {
@@ -284,8 +129,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
         }).catch(() => window.alert("Not a word"))
 
+        
 
     }
+
+    
 
     function createSquares() {
         const gameBoard = document.getElementById("board");
@@ -299,6 +147,18 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    function handleDeleteLetter() {
+        const currentWordArr = getCurrentWordArr();
+        const removedLetter = currentWordArr.pop();
+    
+        guessedWords[guessedWords.length - 1] = currentWordArr;
+    
+        const lastLetterEl = document.getElementById(String(availableSpace - 1));
+    
+        lastLetterEl.textContent = "";
+        availableSpace = availableSpace - 1;
+      }
+
     for (let i = 0; i < keys.length; i++) {
         keys[i].onclick = ({ target }) => {
             const letter = target.getAttribute('data-key');
@@ -307,7 +167,23 @@ document.addEventListener("DOMContentLoaded", () => {
                 handleSubmitWord();
                 return;
             }
+
+            if(letter === 'del'){
+                handleDeleteLetter();
+                return;
+
+            }
             updateGuessedWords(letter);
         };
     }
+
 });
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square, i) => {
+        square.textContent = guessedWords.flat()[i] || "";
+    });
+
+
+
+
+
